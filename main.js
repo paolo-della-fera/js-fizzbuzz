842d5c20bb72d5910fbbcbcf858b8bf9f41b3031
paolo-della-fera <paolodf2005@gmail.com>
2026-02-03 15:48:00 +0100

    add multiples of 3 and 5
    If they have multiples in common, they are printed to the console as "FizzBuzz"

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 // inizio creandomi il ciclo da 1 a 100 con For 
-for (let i = 1; i <= 100; i++)
+for (let i = 1; i <= 100; i++) {
+
+    // inizio trovandomi prima il multipli che hanno in comune 3 e 5
+
+    // SE hanno multipli in comune vengono stampati in console come "FizzBuzz" 
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+}
