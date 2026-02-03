0755c5aab11cd929d73a0e2b4039f79aa836e5af
paolo-della-fera <paolodf2005@gmail.com>
2026-02-03 15:53:44 +0100

    add multiples of 3
    if the number is a multiple of 3 it is replaced by the word "Fizz"

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 for (let i = 1; i <= 100; i++) {
 
     // inizio trovandomi prima il multipli che hanno in comune 3 e 5
-
     // SE hanno multipli in comune vengono stampati in console come "FizzBuzz" 
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     }
+
+    // identifico i multipli solo di 3 
+    // SE il numero è multiplo di 3 viene sostituito dallla parola "Fizz"
+    else if (i % 3 == 0) {
+        console.log('Fizz');
+    }
+
 }
